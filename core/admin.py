71984c93f91d0ebcab92d8c2a5adf7e10540c993
Jan Unar <johnunar@gmail.com>
2023-01-19 02:01:41 +0100

    Added model admins

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import (
+    AttributeName,
+    AttributeValue,
+    Attribute,
+    Product,
+    ProductAttribute,
+    Image,
+    ProductImage,
+    Catalog,
+)
+
+
+@admin.register(AttributeName)
+class AttributeNameAdmin(admin.ModelAdmin):
+    list_display = ("name", "code", "display")
+
+
+@admin.register(AttributeValue)
+class AttributeValueAdmin(admin.ModelAdmin):
+    list_display = ("value",)
+
+
+@admin.register(Attribute)
+class AttributeAdmin(admin.ModelAdmin):
+    list_display = ("name", "value")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "currency", "is_published")
+
+
+@admin.register(ProductAttribute)
+class ProductAttributeAdmin(admin.ModelAdmin):
+    list_display = ("product", "attribute")
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ("name", "url")
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ("product", "image")
+
+
+@admin.register(Catalog)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ("name",)
