586e5bf665c27fba25d63e4a521080e4ea78a0f9
Jan Unar <johnunar@gmail.com>
2023-01-19 02:29:41 +0100

    Added static files settings

--- whysapi/settings.py
+++ whysapi/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "whysapi.unar.dev"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "whys.unar.dev"]
 
 # Application definition
 
@@ -113,7 +113,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
+
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
